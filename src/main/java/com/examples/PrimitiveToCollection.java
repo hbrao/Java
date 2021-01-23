8a5104880765c2597d77f2a2ed62c162badd7714
hbrao <hanu.16aug@outlook.com>
2021-01-23 10:25:20 -0800

    Update PrimitiveToCollection.java

--- src/main/java/com/examples/PrimitiveToCollection.java
+++ src/main/java/com/examples/PrimitiveToCollection.java
@@ -6,26 +6,39 @@ import static java.util.stream.Collectors.*;
 
 public class PrimitiveToCollection {
     public static void main(String[] args) {
+
         //Method 1
+        List<Integer> aL1 = new ArrayList<>(List.of(1, 2, 2, 3, 4, 4, 5));
+        System.out.println("List contents: " + aL1);
+        List<Integer> aL1sub1 = aL1.subList(0, 3);
+        aL1sub1.set(0, 1000);
+        System.out.println("List contents: " + aL1);
+        System.out.println("Sub list contents: " + aL1sub1);
+
+        //Method 2
         int[] data = {1,2,2,3,4,4,5};
+        //NOTE: <Integer> on LHS won't work as data is an array of primitives
+        List arrList1 = new ArrayList<>(Arrays.asList(data));
+        arrList1.add(100);
+        System.out.println("Array List = "+arrList1);
 
-        List arrList = IntStream.of(data).boxed().collect(toList());
-        arrList.add(100);
-        System.out.println("Array List = "+arrList);
+        List<Integer> arrList2 = IntStream.of(data).boxed().collect(toList());
+        arrList2.add(100);
+        System.out.println("Array List = "+arrList2);
 
-        List linkedList = IntStream.of(data).boxed().collect(toCollection(LinkedList::new));
+        List<Integer> linkedList = IntStream.of(data).boxed().collect(toCollection(LinkedList::new));
         linkedList.add(111);
         System.out.println("Linked List = "+linkedList);
 
-        Set set = IntStream.of(data).boxed().collect(toSet());
+        Set<Integer> set = IntStream.of(data).boxed().collect(toSet());
         set.add(222);
         System.out.println("Set = "+set);
 
-        SortedSet sortedSet = IntStream.of(data).boxed().collect(toCollection(TreeSet::new));
+        SortedSet<Integer> sortedSet = IntStream.of(data).boxed().collect(toCollection(TreeSet::new));
         set.add(222);
         System.out.println("Sorted Set = "+sortedSet);
 
-        Map hm1 = IntStream.of(data).boxed().distinct().collect(toMap((i) -> i.intValue(), (i) -> i.toString()));
+        Map<Integer,String> hm1 = IntStream.of(data).boxed().distinct().collect(toMap((i) -> i.intValue(), (i) -> i.toString()));
         System.out.println("Map 1 Distinct keys= "+hm1);
 
         Map<Integer, Long> hm2= IntStream.of(data).boxed().collect(groupingBy(Integer::intValue,counting()));
@@ -47,13 +60,5 @@ public class PrimitiveToCollection {
         ));
         System.out.println("Map 5 (sorted reverse) Group repeated values into Set");
         hm5.entrySet().forEach( (Map.Entry<Integer,Set<Integer>> e) -> System.out.println(e.getKey() + " : " + e.getValue()) );
-
-        //Method 2
-        List<Integer> aL1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        System.out.println("List contents: " + aL1);
-        List<Integer> aL1sub1 = aL1.subList(0, 3);
-        aL1sub1.set(0, 1000);
-        System.out.println("List contents: " + aL1);
-        System.out.println("Sub list contents: " + aL1sub1);
     }
 }
