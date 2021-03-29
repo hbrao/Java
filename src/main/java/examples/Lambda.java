9fa2d51ec921f977b23a6b582cc682e33355c3ee
hbrao <hanu.16aug@outlook.com>
2021-03-29 00:14:06 -0700

    Update Lambda.java

--- src/main/java/examples/Lambda.java
+++ src/main/java/examples/Lambda.java
@@ -3,11 +3,13 @@ package examples;
 public class Lambda {
     public static void main(String[] args) {
         //Anonymous inner class
+        //NOTE m will be of type String and lambda function should return void.
         GreetingMessage gm = (m) -> System.out.println(m);
         gm.greetMessage("Say Hello");
 
         //Method reference.
-        //NOTE: Argument and return type should match with functional interface method.
+        //NOTE: calculateArea argument and return type should match 
+        //      with functional interface method.
         Shape sq1 = Square::calculateArea;
         System.out.println("Area of sq1 = "+sq1.getArea(new Square(4.0f)));
     }
