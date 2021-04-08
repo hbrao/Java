b81ffadf5dd8290b1adf7782451c355ff01bec29
hbrao <hanu.16aug@outlook.com>
2021-04-08 00:04:25 -0700

    Refactoring

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,17 +1,45 @@
 import org.junit.*;
 import org.junit.runner.*;
 
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
         JUnitCore.main("Solution");
     }
 
-    public String foo() {
-        return "Foo";
+    public void reverseString(char[] in) {
+
+        if ( in == null || in.length <= 1 ) return;
+
+        Integer i = 0;
+        Integer j = in.length - 1;
+        while ( i <= j ){
+            //Swap
+            swap(in, i, j);
+
+            i += 1;
+            j -= 1;
+        }
+    }
+
+    public void swap(char[] in, Integer i, Integer j) {
+        char tmp = in[i];
+        in[i] = in[j];
+        in[j] = tmp;
     }
 
     @Test
-    public void testFoo() {
-        Assert.assertTrue(this.foo() == "Foo");
+    public void testReverseString() {
+        List<String> input = List.of("hello", "united state", "abba");
+        for (String s : input) {
+            char[] in = s.toCharArray();
+            String expected = new StringBuilder(s).reverse().toString();
+
+            reverseString(in);
+            String reversed = new String(in);
+
+            Assert.assertTrue(reversed, expected.equals(reversed));
+        }
     }
 }
