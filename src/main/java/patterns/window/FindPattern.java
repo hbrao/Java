20dae65f77d24adfb17bc7452f71b801080e92b7
hbrao <hanu.16aug@outlook.com>
2021-06-26 09:45:50 -0700

    Window matching.

--- src/main/java/patterns/window/FindPattern.java
+++ src/main/java/patterns/window/FindPattern.java
@@ -40,6 +40,31 @@ public class FindPattern {
         return false;
     }
 
+    public static List<Integer> findWordConcatenation(String str, String[] words) {
+        List<Integer> resultIndices = new ArrayList<Integer>();
+        Map<String,Integer> wordFreq = Arrays.stream(words).collect(Collectors.toMap( w -> w, w -> 1, (v1, v2) -> v1 + v2));
+        Integer wl = words[0].length(), windowStart = 0, matched = 0;
+        for(Integer windowEnd = 0; windowEnd < str.length(); windowEnd += wl) {
+            String lastWord = str.substring(windowEnd, Math.min(str.length(), windowEnd + wl));
+            if( wordFreq.containsKey(lastWord) ) {
+                Integer newValue = wordFreq.merge(lastWord, -1, (v1, v2) -> v1 + v2);
+                if( newValue == 0 ) matched += 1;
+            }
+            if( matched == wordFreq.size() ){
+                resultIndices.add(windowStart);
+            }
+            if( windowEnd + wl - windowStart + 1 >= words.length * wl ) {
+                String firstWord = str.substring(windowStart, Math.min(str.length(), windowStart + wl));
+                if( wordFreq.containsKey(firstWord) ) {
+                    if( wordFreq.get(firstWord) >= 0 ) matched -= 1;
+                    wordFreq.merge(firstWord, 1, (v1, v2) -> v1 + v2);
+                }
+                windowStart += wl;
+            }
+        }
+        return resultIndices;
+    }
+
     @Test
     public void testAPIs() {
         Assert.assertEquals("Incorrect output ", true, hasPermutedPattern("oidbcaf", "abc"));
@@ -48,5 +73,8 @@ public class FindPattern {
         Assert.assertEquals("Incorrect output ", true, hasPermutedPattern("aaacb", "abc"));
         Assert.assertEquals("Incorrect output ", false, hasPermutedPattern("aaadcb", "aabc"));
         Assert.assertEquals("Incorrect output ", true, hasPermutedPattern("aaacb", "aabc"));
+
+        Assert.assertArrayEquals("Incorrect indices ", new Integer[]{0, 3}, findWordConcatenation("catfoxcat", new String[] { "cat", "fox" }).toArray());
+        Assert.assertArrayEquals("Incorrect indices ", new Integer[]{3}, findWordConcatenation("catcatfoxfox", new String[] { "cat", "fox" }).toArray());
     }
 }
