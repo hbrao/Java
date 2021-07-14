76c29c4c0e525e05eae694daeedfb8aadb4468a5
hbrao <hanu.16aug@outlook.com>
2021-07-13 19:43:27 -0700

    Refactoring.

--- src/main/java/patterns/graph/TopologicalSort.java
+++ src/main/java/patterns/graph/TopologicalSort.java
@@ -122,11 +122,11 @@ class TopologicalSort {
 
     public static void main(String[] args) {
 
-        //       (1)     (2)
+        //       (1)     (3)
         //
         //  (0)               (5)
         //
-        //       (3)     (4)
+        //       (2)     (4)
 
         int[][] graph =  new int[][]{ new int[]{0, 1}, new int[]{1, 2}, new int[]{0, 3}, new int[]{0, 5}, new int[]{1, 2},
                                       new int[]{2, 4}, new int[]{3, 2}, new int[]{3, 5}, new int[]{5, 4} };
